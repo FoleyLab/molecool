4b889a956dc1ee32d3a7beebaa1051f83b2b2675
FoleyLab <foleyj10@wpunj.edu>
2021-05-27 16:18:40 -0400

    restructured the package

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,8 +4,12 @@ A python package for analyzing and visualizing molecular files.  For molssi work
 """
 
 # Add imports here
-from .functions import *
+from .functions import canvas, zen
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_bond_histogram, draw_molecule
 
+import molecool.io 
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
