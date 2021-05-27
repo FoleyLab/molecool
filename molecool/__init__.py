565f2491fee7d4c57c9eb7f128d478d0f0299636
FoleyLab <foleyj10@wpunj.edu>
2021-05-27 16:25:08 -0400

    changed the init

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -9,7 +9,6 @@ from .measure import calculate_distance, calculate_angle
 from .molecule import build_bond_list
 from .visualize import draw_bond_histogram, draw_molecule
 
-import molecool.io 
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
